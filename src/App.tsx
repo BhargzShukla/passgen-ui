d3d7f326dcb8ed26fe247c629c3b929c4e274116
Bhargav Shukla <shukla.bhargav.k@gmail.com>
2021-10-11 02:19:22 +0530

    Add author and project details

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Box, Divider, Heading } from "@chakra-ui/react";
+import { Box, Divider, Heading, Link, Text } from "@chakra-ui/react";
 import * as React from "react";
 import { PasswordGeneratorForm } from "./components/password-generator";
 
@@ -6,6 +6,15 @@ function App() {
   return (
     <Box rounded="lg" borderWidth={1} paddingX={4} paddingY={6} marginY={8}>
       <Heading marginBottom={6}>Password Generator</Heading>
+      <Text>
+        Built by{" "}
+        <Link href="https://bhargavshukla.com" color="blue.500" isExternal>
+          Bhargav Shukla.
+        </Link>{" "}
+        Built using React and .NET. Website hosted on{" "}
+        <Link href="https://vercel.com">Vercel.</Link> Generation logic hosted
+        on Azure Functions.
+      </Text>
       <Divider marginY={6} />
       <PasswordGeneratorForm />
     </Box>
